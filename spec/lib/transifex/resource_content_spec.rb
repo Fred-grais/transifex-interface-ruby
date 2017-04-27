da0fa5096b64eda0c1de9e7cf210851f13db20a7
Monica Giambitto <monica.giambitto@freeletics.com>
2017-04-27 16:52:36 +0200

    use VCR for update resource content tests

--- spec/lib/transifex/resource_content_spec.rb
+++ spec/lib/transifex/resource_content_spec.rb
@@ -1,6 +1,6 @@
 require_relative "../../spec_helper"
 
-describe Transifex::ResourceComponents::Content do 
+describe Transifex::ResourceComponents::Content do
   before(:all) do
     @project = Transifex::Project.new("projet-de-test-1")
     @resource = @project.resource("test")
@@ -18,7 +18,7 @@ describe Transifex::ResourceComponents::Content do
       expect{ resource_content = @resource.content.fetch }.to_not raise_error
       expect(resource_content).to be_a_kind_of(Hash)
       expect(resource_content.keys).to contain_exactly("content", "mimetype")
-    end 
+    end
 
     it "should retrieve the resource content as a file" do
       resource_content = nil
@@ -26,15 +26,27 @@ describe Transifex::ResourceComponents::Content do
       expect{ resource_content = @resource.content.fetch_with_file(path_to_file) }.to_not raise_error
       file_exist = File.file?(path_to_file)
       expect(file_exist).to eq(true)
-    end 
+    end
   end
 
   describe "Update" do
-    it "should not raise an error and update the resource content" do
-      expect{ @resource.content.update({:i18n_type => "YAML", :content => get_yaml_source_trad_file_path('en')}) }.to_not raise_error
+    let(:project) { Transifex::Project.new("ruby-client") }
+
+    it "should update a resource using a file" do
+      file_resource = project.resource("test")
+
+      VCR.use_cassette "resource/update_yml" do
+        expect { file_resource.content.update(i18n_type: "YAML", content: get_yaml_source_trad_file_path('eo')) }.to_not raise_error
+      end
     end
-    it "should raise an error if updated resource content is of a different type than the previous" do
-      expect{ @resource.content.update({:i18n_type => "TXT", :content => get_yaml_source_trad_file_path('en')}) }.to raise_error(Transifex::Error)
+
+    it "should raise an error if the mime type of the resource to be updated is different than the one defined in the project for the resource" do
+      file_resource = project.resource("test")
+
+      VCR.use_cassette "resource/update_with_wrong_mimetype" do
+        expect { file_resource.content.update(i18n_type: "TXT", content: "") }.to raise_error(Transifex::Error)
+          .with_message("You must use the mimetype YML to upload a new resource file")
+      end
     end
   end
 end
