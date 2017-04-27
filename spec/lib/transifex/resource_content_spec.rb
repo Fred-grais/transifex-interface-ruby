572d779721b111b623aa6b8b20c009564245396d
Monica Giambitto <monica.giambitto@freeletics.com>
2017-04-27 17:29:05 +0200

    use VCR to fetch resource content and refactor spec

--- spec/lib/transifex/resource_content_spec.rb
+++ spec/lib/transifex/resource_content_spec.rb
@@ -1,12 +1,10 @@
 require_relative "../../spec_helper"
 
 describe Transifex::ResourceComponents::Content do
-  before(:all) do
-    @project = Transifex::Project.new("projet-de-test-1")
-    @resource = @project.resource("test")
-  end
+  let(:project) { Transifex::Project.new("ruby-client") }
+  let(:resource) { project.resource("test") }
 
-  describe "Instanciation" do
+  describe "Instantiation" do
     it "should raise an error when no parameters given" do
       expect{ Transifex::ResourceComponents::Content.new }.to raise_error(ArgumentError)
     end
@@ -14,37 +12,32 @@ describe Transifex::ResourceComponents::Content do
 
   describe "Fetch" do
     it "should retrieve the resource content as a hash" do
-      resource_content = nil
-      expect{ resource_content = @resource.content.fetch }.to_not raise_error
-      expect(resource_content).to be_a_kind_of(Hash)
-      expect(resource_content.keys).to contain_exactly("content", "mimetype")
+      VCR.use_cassette "resource/fetch_content_as_hash" do
+        expect(resource.content.fetch).to eq(
+          {"content" => %Q{eo:\n  test_string: "test string"\n}, "mimetype" => "text/plain"}
+        )
+      end
     end
 
     it "should retrieve the resource content as a file" do
-      resource_content = nil
-      path_to_file = File.dirname(__FILE__) + "/../yaml/resource_content_test.yml"
-      expect{ resource_content = @resource.content.fetch_with_file(path_to_file) }.to_not raise_error
-      file_exist = File.file?(path_to_file)
-      expect(file_exist).to eq(true)
+      VCR.use_cassette "resource/fetch_content_as_file" do
+        resource.content.fetch_with_file("fetched.yml")
+      end
+
+      expect(File.exist?("fetched.yml")).to be true
     end
   end
 
   describe "Update" do
-    let(:project) { Transifex::Project.new("ruby-client") }
-
     it "should update a resource using a file" do
-      file_resource = project.resource("test")
-
-      VCR.use_cassette "resource/update_yml" do
-        expect { file_resource.content.update(i18n_type: "YAML", content: get_yaml_source_trad_file_path('eo')) }.to_not raise_error
+      VCR.use_cassette "resource/update_content_yml" do
+        expect { resource.content.update(i18n_type: "YAML", content: get_yaml_source_trad_file_path('eo')) }.to_not raise_error
       end
     end
 
     it "should raise an error if the mime type of the resource to be updated is different than the one defined in the project for the resource" do
-      file_resource = project.resource("test")
-
-      VCR.use_cassette "resource/update_with_wrong_mimetype" do
-        expect { file_resource.content.update(i18n_type: "TXT", content: "") }.to raise_error(Transifex::Error)
+      VCR.use_cassette "resource/update_content_with_wrong_mimetype" do
+        expect { resource.content.update(i18n_type: "TXT", content: "") }.to raise_error(Transifex::Error)
           .with_message("You must use the mimetype YML to upload a new resource file")
       end
     end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -43,6 +43,9 @@ end
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+  config.after(:suite) do # or :each or :all
+    File.delete("fetched.yml") if File.exists?("fetched.yml")
+  end
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin
